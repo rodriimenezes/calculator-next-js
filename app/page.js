46956ad117e86437242ed634e3b17f173ce14c7a
Rodrigo Menezes <rodriisabatico@gmail.com>
2025-02-28 15:15:34 -0300

    added basic css

--- app/page.js
+++ app/page.js
@@ -6,35 +6,35 @@ export default function Home() {
   const [resultado, setResultado] = useState(0)
 
   return (
-    <main className="grid justify-center items-center grid-rows-2">
-      <div>
+    <main>
+      <div className="display">
         {resultado}
       </div>
-      <div>
+      <div className="grid grid-cols-4 gap-4 m-4">
         <button>C</button>
         <button>()</button>
         <button>%</button>
-        <button>/</button><br></br>
+        <button>/</button>
 
         <button>7</button>
         <button>8</button>
         <button>9</button>
-        <button>X</button> <br></br>
+        <button>X</button> 
 
         <button>4</button>
         <button>5</button>
         <button>6</button>
-        <button>-</button> <br></br>
+        <button>-</button> 
 
         <button>1</button>
         <button>2</button>
         <button>3</button>
-        <button>+</button> <br></br>
+        <button>+</button> 
 
         <button>+/-</button>
         <button>0</button>
         <button>,</button>
-        <button>=</button> <br></br>
+        <button>=</button> 
       </div>
     </main>
   );
